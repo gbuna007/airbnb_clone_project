f91dc07a3f6bbd7e7a06e7eee8da73bb1d84fe6c
Grace Bunardi <gbunardi@hotmail.com>
2022-08-23 15:40:27 +0800

    add set flat private method in flats controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,17 @@
 class FlatsController < ApplicationController
   skip_before_action :authenticate_user!, only: %I[show index]
+  before_action :set_flat, only: [:show]
 
   def index
     @flats = Flat.all
   end
 
   def show
+  end
+
+  private
+
+  def set_flat
     @flat = Flat.find(params[:id])
   end
 end
