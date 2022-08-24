ba6b7a28aed096a1bb0ae86d06c4b77e37fb9c13
Grace Bunardi <gbunardi@hotmail.com>
2022-08-24 13:29:18 +0800

    added availability status column to flats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_045615) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_052752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_045615) do
     t.date "avail_dates"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "availability_status"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
