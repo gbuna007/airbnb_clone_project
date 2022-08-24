5a6114d96768bc0f035404483027620f5d5949f5
Alfred Tay <twx.alfred@gmail.com>
2022-08-24 16:04:49 +0800

    added css and views

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem "jbuilder"
 
 gem "pundit"
 
+gem "dotenv-rails", groups: [:development, :test]
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,38 @@ sarina = User.create(email: "sarina.y.ke@gmail.com", password: "sarina123", firs
 laura = User.create(email: "laura.kim@gmail.com", password: "laura123", first_name: "Laura", last_name: "Williams")
 anushka = User.create(email: "anushka.p@gmail.com", password: "anushka123", first_name: "Anushka", last_name: "Vodivya")
 
-mansion = Flat.new(name: "Mansion", location: "Beverly Hills", price: 10000, occupants: 10)
+mansion = Flat.new(name: "California Mansion", location: "Beverly Hills", price: 10000, occupants: 10)
 mansion.user = sarina
 mansion.save
 
-house = Flat.new(name: "House", location: "NYC", price: 7000, occupants: 2)
+mansion1 = Flat.new(name: "Luxurious apartment", location: "NYC", price: 100000, occupants: 6)
+mansion1.user = sarina
+mansion1.save
+
+mansion2 = Flat.new(name: "Landed property with a jamming studio", location: "SG", price: 50000, occupants: 8)
+mansion2.user
+mansion2.save
+
+house = Flat.new(name: "House at the heart of the city", location: "NYC", price: 7000, occupants: 2)
 house.user = laura
 house.save
 
-apartment = Flat.new(name: "Apartment", location: "SG", price: 1000, occupants: 3)
+house1 = Flat.new(name: "Countryside house just outside the city", location: "Dublin", price: 3000, occupants: 5)
+house1.user = laura
+house1.save
+
+house2 = Flat.new(name: "Rural seaside hut", location: "Bali", price: 7000, occupants: 6)
+house2.user = laura
+house2.save
+
+apartment = Flat.new(name: "Small 3-room Apartment", location: "SG", price: 2500, occupants: 3)
 apartment.user = anushka
 apartment.save
+
+apartment1 = Flat.new(name: "65sq medium room", location: "Budapest", price: 1200, occupants: 2)
+apartment1.user = anushka
+apartment1.save
+
+apartment2 = Flat.new(name: "Small Room with a nearby gym", location: "Seoul", price: 1000, occupants: 1)
+apartment2.user = anushka
+apartment2.save
